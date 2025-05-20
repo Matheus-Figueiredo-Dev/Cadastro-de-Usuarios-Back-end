a44b8746b7545d7f952fe9af58fe85ea9cdcfbf8
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-05-20 17:12:10 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+});
+
+app.post('/users', async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    },
+  });
+  res.status(201).json(user);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    },
+  });
+  res.status(200).json(user);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ message: 'User deleted successfully!' });
+});
+
+app.listen(3002);
