ea18dac3fab9e8568aa35b76f262cc9d20c7ca85
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-05-21 14:23:02 -0300

    Adicionando CORS

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 import express from 'express';
 
 const prisma = new PrismaClient();
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany();
